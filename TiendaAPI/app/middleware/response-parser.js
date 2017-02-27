7f810e4065bd23b6092967e2c1fdf192053c9160
Roberto Carlos Trujillo Torres <rko.bismarck@gmail.com>
2017-02-27 17:33:46 -0600

    Update response-parser.js

--- TiendaAPI/app/middleware/response-parser.js
+++ TiendaAPI/app/middleware/response-parser.js
@@ -7,3 +7,25 @@ function responseParser(req,resp,next){
 };
 
 module.exports = responseParser;
+
+
+
+
+var namespace = "datos";
+
+var Objeto = {
+	Nombre : 'ParametroA',
+	Descripcion : "ParametroB",
+	Imagenes : ["a.jpg","b.jpg","c.jpg","d.jpg"],
+	Stock : 1,
+	Categorias : [1,2,3,4,5],
+	It : 1,
+	Ites : ['a','b','c']
+}
+
+var transformarObjeto = (Objeto) => Object.keys(Objeto).map( x => Array.isArray(Objeto[x]) ? (Objeto[x] = { [namespace] : Objeto[x] } ): null);
+
+
+transformarObjeto(Objeto)
+
+console.log(Objeto)
