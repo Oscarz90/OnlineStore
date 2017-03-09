51efbaf8158da216ae2a078855c6193027215836
Roberto Carlos Trujillo Torres <rko.bismarck@gmail.com>
2017-03-08 18:20:30 -0600

    Se agrega la funcionalidad que permite recursividad en la funcion

--- TiendaAPI/app/middleware/response-parser.js
+++ TiendaAPI/app/middleware/response-parser.js
@@ -2,13 +2,16 @@
 var Respuesta = require('../entidades/respuesta');
 
 let namespace="data";
-const formatearResultados = (resultados)=>Object.keys(resultados).map(atributo=>Array.isArray(resultados[atributo])?(resultados[atributo]={[namespace]:resultados[atributo], lenght:resultados[atributo].length}):resultados[atributo]);
+const formatearResultados = (resultados)=>Object.keys(resultados).map(atributo=>Array.isArray(resultados[atributo])?(resultados[atributo]={[namespace]:resultados[atributo],lenght:resultados[atributo].length}):typeof resultados[atributo] === 'object' ?  formatearResultados(resultados[atributo]):null );
+
 
 function responseParser(req,resp){
   formatearResultados(resp.locals.results);
   const resultado = new Respuesta(resp.locals.metadata,resp.locals.results);
   resp.status(resultado.metadata.status).jsonp(resultado);
 };
+
+
 
 
 
